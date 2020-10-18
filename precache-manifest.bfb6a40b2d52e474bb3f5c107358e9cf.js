058a74a19f25794503a71d9075643401ff38a78c
eugens90 <66322185+EugenS90@users.noreply.github.com>
2020-10-18 13:42:49 +0300

    Add files via upload

---
+++ precache-manifest.bfb6a40b2d52e474bb3f5c107358e9cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ebb652d5e2ea49cfa631c0bc9032639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71462e9e5c6102a91413",
+    "url": "/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "8b44a60789744b0f33c9",
+    "url": "/static/css/main.0ab3b10b.chunk.css"
+  },
+  {
+    "revision": "71462e9e5c6102a91413",
+    "url": "/static/js/2.e62d6d1d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e62d6d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b44a60789744b0f33c9",
+    "url": "/static/js/main.3b92548d.chunk.js"
+  },
+  {
+    "revision": "960c75734a15ff033a55",
+    "url": "/static/js/runtime-main.8f686904.js"
+  },
+  {
+    "revision": "304e5212f4da364e03f792b6d5fdd200",
+    "url": "/static/media/logo.304e5212.svg"
+  }
+]);
