287846954d83a538cf998461142125aab9d3d352
EugenS90 <66322185+EugenS90@users.noreply.github.com>
2020-10-18 12:45:32 +0300

    Add files via upload

--- src/bookmarks/Bookmarks.js
+++ src/bookmarks/Bookmarks.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import './Bookmarks.css';
+import logo from './logo.svg'
 
 class Bookmarks extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+
+    }
   }
   render() {
     return (
       <div>
-        <p>Bookmarks</p>
+        <h3>There are your bookmarks</h3>
+        <div className='all__cards'>
+          <div className='card'>
+            <h4>BazaIT</h4>
+            <img src={logo} />
+            <p>JS developer</p>
+            <button className='btn btn-warning' onClick={this.ignore}>Delete</button>
+          </div>
+          <div className='card'>
+            <h4>BazaIT</h4>
+            <img src={logo} />
+            <p>JS developer</p>
+            <button className='btn btn-warning' onClick={this.ignore}>Delete</button>
+          </div>
+          <div className='card'>
+            <h4>BazaIT</h4>
+            <img src={logo} />
+            <p>JS developer</p>
+            <button className='btn btn-warning' onClick={this.ignore}>Delete</button>
+          </div>
+          <div className='card'>
+            <h4>BazaIT</h4>
+            <img src={logo} />
+            <p>JS developer</p>
+            <button className='btn btn-warning' onClick={this.ignore}>Delete</button>
+          </div>
+        </div>
       </div>
     );
   }
